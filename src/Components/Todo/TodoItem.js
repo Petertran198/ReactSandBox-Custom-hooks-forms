6150a1700e7f06bbfec84b27ea39bbe3d535c2ce
petertran98 <Petertran198@gmail.com>
2021-03-14 14:27:55 -0500

    install material icon and added delete and edit icon to each TodoItem

--- src/Components/Todo/TodoItem.js
+++ src/Components/Todo/TodoItem.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import {Delete, Edit} from '@material-ui/icons';
+import IconButton from '@material-ui/core/IconButton';
 
 const TodoItem = (props) => {
     
@@ -14,6 +16,13 @@ const TodoItem = (props) => {
             <ListItemText>
                 {props.todo.task}
             </ListItemText>
+            <IconButton>
+                <Edit/>
+            </IconButton>
+            <IconButton>
+                <Delete/>
+            </IconButton>            
+
         </ListItem> 
     )
 }
